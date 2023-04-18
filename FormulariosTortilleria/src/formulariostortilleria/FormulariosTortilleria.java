82abb98a477aaa577dd1d7613982c5de42a31488
José Manuel Jimeno Islas <99367439+JManuelJimeno@users.noreply.github.com>
2023-04-17 20:32:28 -0600

    Funcionalidad terminada

--- FormulariosTortilleria/src/formulariostortilleria/FormulariosTortilleria.java
+++ FormulariosTortilleria/src/formulariostortilleria/FormulariosTortilleria.java
@@ -4,6 +4,13 @@
  */
 package formulariostortilleria;
 
+import vistas.Inicio;
+import vistas.Inventario;
+import vistas.Principal;
+import vistas.Proveedor;
+import vistas.Usuarios;
+import vistas.Ventas;
+
 /**
  *
  * @author ramirohb
@@ -15,6 +22,14 @@ public class FormulariosTortilleria {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Inicio principal = new Inicio();
+        principal.setVisible(true);
+        
+        Inventario inv1 = new Inventario();
+        Principal p1 = new Principal();
+        Proveedor pro1 = new Proveedor();
+        Usuarios usu = new Usuarios();
+        Ventas ven = new Ventas();
     }
     
 }
